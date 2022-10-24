3b9296e4d6cc9c9e90d8d1e1790e97493404fd8d
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-10-24 12:21:11 -0300

    Links arrumados

--- src/ComponentsMUI/menu.tsx
+++ src/ComponentsMUI/menu.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Menu, { MenuProps } from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
- 
-
-
-
-
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Menu, { MenuProps } from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Link } from "react-router-dom";
 
 const StyledMenu = withStyles({
   paper: {
-    border: '1px solid #d3d4d5',
+    border: "1px solid #d3d4d5",
   },
 })((props: MenuProps) => (
   <Menu
     elevation={0}
     getContentAnchorEl={null}
     anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'center',
+      vertical: "bottom",
+      horizontal: "center",
     }}
     transformOrigin={{
-      vertical: 'top',
-      horizontal: 'center',
+      vertical: "top",
+      horizontal: "center",
     }}
     {...props}
   />
@@ -30,9 +26,9 @@ const StyledMenu = withStyles({
 
 const StyledMenuItem = withStyles((theme) => ({
   root: {
-    '&:focus': {
+    "&:focus": {
       backgroundColor: theme.palette.primary.main,
-      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
         color: theme.palette.common.white,
       },
     },
@@ -51,33 +47,25 @@ export default function CustomizedMenus() {
   };
 
   return (
-    <div className='h-24 bg-blue-800 flex items-center pr-2 pl-2'>
-      <button
-        onClick={handleClick}
-      >
+    <div className="h-24 bg-blue-800 flex items-center pr-2 pl-2">
+      <button onClick={handleClick}>
         ola
-        <img src='' alt=''/>
+        <img src="" alt="" />
       </button>
       <StyledMenu
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <StyledMenuItem>
-          <a href="/" >
-            Teste
-          </a>
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <a href="/" >
-            Teste
-          </a>
-        </StyledMenuItem>
-        <StyledMenuItem>
-          <a href="/clientes"  className='w-48'>
-            Lista de Clientes
-          </a>
-        </StyledMenuItem>
+        <Link to="/">
+          <StyledMenuItem>Teste</StyledMenuItem>
+        </Link>
+        <Link to={"/"}>
+          <StyledMenuItem>Teste</StyledMenuItem>
+        </Link>
+        <Link to="/clientes" className="w-48">
+          <StyledMenuItem>Lista de Clientes</StyledMenuItem>
+        </Link>
       </StyledMenu>
     </div>
   );
