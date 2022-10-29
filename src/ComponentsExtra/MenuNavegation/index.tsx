1f875548c896874ca05ca02b4a2b4f0db23f7172
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-10-29 08:00:50 -0300

    Ajustado css do menu e adiconado tabela clientes

--- src/ComponentsExtra/MenuNavegation/index.tsx
+++ src/ComponentsExtra/MenuNavegation/index.tsx
@@ -2,15 +2,32 @@ import { Link } from "react-router-dom";
 
 const MenuNavegation = () => {
   return (
-    <div className="absolute bg-red-500 p-4 m-0 w-[380px]">
-      <ul>
+   
+   <div className="absolute p-4 m-0 w-[380px] left-0 bg-blue-800">
+   <div>
+     <h1 className="text-white text-2xl leading-7">GESTÃO DE CLIENTES</h1>
+      <ul className="pt-4 pb-12">
         <li>
-          <Link to={"/"}>Home</Link>
+        <Link className="text-xl text-white opacity-50 font-light hover:opacity-100" to={"/clientes"}>Lista de clientes</Link>
         </li>
         <li>
-          <Link to={"/clientes"}>Lista de Clientes</Link>
+        <Link className="text-xl text-white opacity-50 font-light hover:opacity-100" to={"/"}>Cadastrar cliente</Link>
         </li>
       </ul>
+      </div>
+
+      <div>
+     <h1 className="text-white text-2xl leading-7">GESTÃO DE VENDAS</h1>
+      <ul className="pt-4 pb-12">
+        <li>
+        <Link className="text-xl text-white opacity-50 font-light hover:opacity-100" to={"/clientes"}>Lista de vendas</Link>
+
+        </li>
+        <li>
+        <Link className="text-xl text-white opacity-50 font-light hover:opacity-100" to={"/"}>Cadastrar venda</Link>
+        </li>
+      </ul>
+      </div>
     </div>
   );
 };
